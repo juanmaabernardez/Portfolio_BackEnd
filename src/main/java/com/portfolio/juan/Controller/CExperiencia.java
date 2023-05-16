dad20d89d51818de7d1514302199420617678f01
juanmaabernardez <juanmartinbernardez@gmail.com>
2023-05-15 21:54:08 -0300

    version backend funcional en localhost:4200

--- src/main/java/com/portfolio/juan/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/juan/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://juan-frontend.web.app")
+@CrossOrigin(origins = {"https://juan-frontend.web.app", "http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/portfolio/juan/JuanApplication.java
+++ src/main/java/com/portfolio/juan/JuanApplication.java
@@ -2,24 +2,12 @@ package com.portfolio.juan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JuanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JuanApplication.class, args);
-	}
-        @Bean
-public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://juan-frontend.web.app").allowedMethods("*").allowedHeaders("*");
-			}
-		};
 	}
 
 }
